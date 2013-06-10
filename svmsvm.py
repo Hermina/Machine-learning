4fb475940374f2dfc70b9522450825512741d006
Hermina <hpetricmaretic@gmail.com>
2013-06-10 19:12:35 +0200

    svm spreman za pokretanje

--- svmsvm.py
+++ svmsvm.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1250 -*-
 import random
 import math
 import copy
@@ -10,30 +11,61 @@ import svm
 from svm import *
 import svmutil
 from svmutil import *
+from grid import *
+import numpy as np
+from sklearn.feature_selection import SelectKBest, chi2
+import itertools
+
+#-----PARAMETRI KOJE JE MOGUCE PODESITI-----#
+
+#zelite li koristiti i bigrame
+KORISTENJE_BIGRAMA = 0
+#odabirom najveca frekvencija = 0, koristi se omjer_pojavljivanja
+NAJVECA_FREKVENCIJA = 1
+#koristenje otkloni nepozeljne
+OTKLONI_NEPOZELJNE = 0
+#koristenje stem-a
+STEM = 0
+#koristenje izbaci stop words
+STOP = 0
+#koristenje k-struke cross validacije
+K = 4
+#koristenje feature selectiona
+FEATURE_SELECTION = 0
+#koristenje sinonima kod pripreme test seta
+SINONIMI = 0
+#broj faetura
+BROJ_FEATURA = 2000
+
+#--------------------------------------------#
+
 
 #izdvajamo rijeci koje se pojavljuju u dokumentu u svrhu odabira featura
 def words_for_feature_selection(document):
     features = []
-    bigram = []
-    bigram.append([w for w in bigrams(document)])
-    document_bigram=[]
-    [document_bigram.extend(w) for w in bigram]
-    document.extend(document_bigram)
+    if(KORISTENJE_BIGRAMA == 1):
+        bigram = []
+        bigram.append([w for w in bigrams(document)])
+        document_bigram=[]
+        [document_bigram.extend(w) for w in bigram]
+        document.extend(document_bigram)
     for word in word_features1:
         features.append(document.count(word))
     return features
 
 #vraca feature u dokumentu
 def document_features(document):
-    bigram = []
-    bigram.append([w for w in bigrams(document)])
-    document_bigram=[]
-    [document_bigram.extend(w) for w in bigram]
-    document.extend(document_bigram)
+    if(KORISTENJE_BIGRAMA == 1):
+        bigram = []
+        bigram.append([w for w in bigrams(document)])
+        document_bigram=[]
+        [document_bigram.extend(w) for w in bigram]
+        document.extend(document_bigram)
     features = {}
     i=0
     for word in word_features:
-        features[i] = document.count(word)
+        if word in document:
+            features[i] = document.count(word)
         i=i+1
     return features
 
@@ -65,17 +97,19 @@ def sinonimi(documents,word_features,dg,gg):
         ex = documents[i]
         index = 0
         for word in ex[0]:
-            if(word not in word_features):
+            if(word not in word_features and type(word) != tuple):
                 if(len(wn.synsets(word)) != 0):
                     x = wn.synsets(word)[0].lemma_names
                     r = [i for i in x if i in word_features]
                     if len(r):
                         ex[0][index] = r[0]
             index = index + 1
-    return test_set
+    return documents
 
 #vracamo rijeci sortirane po omjeru pojavljivanja u subjektivnim i objektivnim recenicama
 #preferiramo rijeci s najmanjim omjerom
+#ako se neka rijec javlja samo u jednoj klasi,definiramo da je njen omjer 0
+#i smatramo ju korisnom
 def omjer_pojavljivanja(documents,dg1,gg1,dg2,gg2):
     mapobj={}
     mapsubj={}
@@ -156,17 +190,19 @@ def ucitavanje(obj, subj):
     f.close()
     documents=[]
     for i in range(5000):
-        documents.append([obj[i][:], 'objective'])
-        documents.append([subj[i][:], 'subjective'])
+        documents.append([obj[i][:], -1])
+        documents.append([subj[i][:], 1])
         
     random.shuffle(documents)
     return documents
 
 #funkcija vraca bigrame koji se pojavljuju u recenicama
-def bigrami(documents):
+def bigrami(documents,dg1,gg1,dg2,gg2):
     bigram = []
     stopwords = nltk.corpus.stopwords.words('english')
-    for i in range(7000):
+    for i in range(dg1,gg1):
+        bigram.append([w for w in bigrams(documents[i][0])])
+    for i in range(dg2,gg2):
         bigram.append([w for w in bigrams(documents[i][0])])
     result = []
     [result.extend(w) for w in bigram]
@@ -177,36 +213,163 @@ def bigrami(documents):
 #parametri:
     #koliko - koliko najinformativnijih featura zelimo izdvojiti
     #funkcija na osnovu chi2 testa odabire najinformativnije feature
-def feature_selection(documents,koliko):
+def feature_selection(documents,koliko,dg1,gg1,dg2,gg2):
     selector = SelectKBest(chi2, koliko)
-    y = [c for (d,c) in documents][:7000]
+    y = []
     X = []
-    for (d,c) in documents[:7000]:
-        X.append(words_for_feature_selection(d))
+    for i in range (dg1,gg1):
+        y.append(documents[i][1])
+        X.append(words_for_feature_selection(documents[i][0]))
+    for i in range (dg2,gg2):
+        y.append(documents[i][1])
+        X.append(words_for_feature_selection(documents[i][0]))
+    X = np.array(X)
+    y = np.array(y)
     selector.fit(X,y)
     m = selector.get_support()
-    return [i[0] for i in izip_longest(word_features1, m[:len(X)], fillvalue=True) if i[1]]
+    return list(itertools.compress(word_features1,m))
 
-#POCINJE KOD
+#prima podatke za treniranje, podatke za validaciju i cross validation parametar (cv)
+#ako je cv!=0, provodi cross validaciju sa cv podskupova, inace koristi skup za validaciju
+#testira algoritam za sve parametre c i g izmedu pocetnic i zavrsnic, odnosno pocetnig i zavrsnig, s korakom korakc, odnosno korakg
+#varijabla tip oznacava tip jezgre
+def nas_grid(tr_classes,tr_set,val_classes,val_set,cv,pocetnic,zavrsnic,korakc,pocetnig,zavrsnig,korakg,tip):
+    dat=open('nas_grid_sinonimi.txt','a+b')
+    maks=0    
+    px=svm_problem(tr_classes,tr_set)
+    i=pocetnic
+    while i<=zavrsnic:
+        j=pocetnig
+        while j<=zavrsnig:
+            c=math.pow(2,i)
+            g=math.pow(2,j)
+            param = svm_parameter()
+            param.kernel_type=tip
+            param.C = c
+            param.gamma = g
+            if (cv):
+                param.nr_fold=cv
+                param.cross_validation=1
+                temp=svm_train(px, param)
+                print >>dat,str(i) + " "+ str(temp)+ " "
+            else:
+                m=svm_train(px, param)
+                temp=svm_predict(val_classes,val_set,m)
+            if(maks<temp):
+                maks=temp
+                cmaks=c
+                gmaks=g
+            j=j+korakg
+        i=i+korakc
+    print >>dat," "
+    dat.close()
+    return [cmaks,gmaks]
+
+#ispunjava datoteku podacima u formatu priladoðenom za funkciju find_parameters
+def ispuni_datoteku(ime_datoteke):
+    sve=[]
+    for i in range(7000):
+        sve.append([train_classes[i],train_set[i]])
+    f = open(ime_datoteke, 'wb')
+    pom=""
+    for d in sve:
+        pom=pom+str(d[0])+" "
+        arr=[]
+        for k in d[1]:
+            arr.append(k)
+        arr.sort()
+        for el in arr:
+            pom=pom+str(el)+":"+str(d[1][el])+" "       
+        print >>f, pom
+        pom=""
+    f.close()
+
+#-----main funkcija-----#
 obj=[]
 subj=[]
 documents=ucitavanje(obj,subj)
-all_words = najveca_frekvencija(obj,subj,0,7000,0,0)
-all_bigrams = bigrami(documents)
-#all_words = [stem(word) for word in all_words]
-word_features = otkloni_nepozeljne(all_words,2000)
-#word_features = all_words.keys()[:2000]
-featuresets = [(document_features(d,1)) for (d,c) in documents]
-classes=[c for (d,c) in documents]
-train_set = featuresets[:7000]
-train_classes=classes[:7000]
-test_classes = classes[7000:]
-test_set = featuresets[7000:]
-px = svm_problem(train_classes,train_set)
-param = svm_parameter()
-param.kernel_type = LINEAR
-param.C = 8
-param.gama = 0.015625
-m = svm_train(px, param)
-svm_predict(test_classes[:],test_set[:],m)
+korak = 10000/K
+if(len(sys.argv) != 10):
+    print "Koristim parametre podesene u kodu"
+    print "Za promjenu ponovno pokrenuti program"
+    print "Unesite svih 9 argumenata komandne linije s vrijednostima 0 ili 1,osim K i BROJ_FEATURA sljedecim redosljedom"
+    print "KORISTENJE_BIGRAMA NAJVECA_FREKVENCIJA OTKLONI_NEPOZELJNE STEM STOP K FEATURE_SELECTION SINONIMI BROJ_FEATURA"
+else:
+   KORISTENJE_BIGRAMA = int(sys.argv[1])
+   NAJVECA_FREKVENCIJA = int(sys.argv[2])
+   OTKLONI_NEPOZELJNE = int(sys.argv[3])
+   STEM = int(sys.argv[4])
+   STOP = int(sys.argv[5])
+   K = int(sys.argv[6])
+   FEATURE_SELECTION = int(sys.argv[7])
+   SINONIMI = int(sys.argv[8])
+   BROJ_FEATURA = int(sys.argv[9])
 
+#-----K-struka cross validacija-----#
+for cv in range(K):
+    word_features1 = []
+    print "izdvajam..."
+    if(NAJVECA_FREKVENCIJA):
+        word_features1 = najveca_frekvencija(obj,subj,0,cv*korak,(cv+1)*korak,10000)
+    else:
+        word_features1 = omjer_pojavljivanja(documents,0,cv*korak,(cv+1)*korak,10000)
+    if(STEM):
+        word_features1 = [stem(word) for word in word_features1]
+    if(STOP):
+        word_features1 = izbaci_stop_words(word_features1)
+    if(OTKLONI_NEPOZELJNE):
+        word_features1 = otkloni_nepozeljne(word_features1,BROJ_FEATURA)
+    if(KORISTENJE_BIGRAMA):
+        all_bigrams = bigrami(documents,0,cv*korak,(cv+1)*korak,10000)
+        if(FEATURE_SELECTION):
+            word_features1.extend(all_bigrams[:5000])
+        else:
+            word_features1.extend(all_bigrams[:2000])
+    if(FEATURE_SELECTION):
+        print "feature selection..."
+        word_features = feature_selection(documents,BROJ_FEATURA,0,cv*korak,(cv+1)*korak,10000)
+    else:
+        word_features = word_features1[:BROJ_FEATURA]
+    if(SINONIMI):
+        print "sinonimi..."
+        documents = sinonimi(documents,word_features,cv*korak,(cv+1)*korak)
+    print "odabir trening i testing seta..."
+    featuresets = [(document_features(d)) for (d,c) in documents]
+    classes=[c for (d,c) in documents]
+    train_set = featuresets[:cv*korak] + featuresets[(cv+1)*korak:]
+    test_set = featuresets[cv*korak:(cv+1)*korak]
+    train_classes=classes[:cv*korak] + classes[(cv + 1)*korak:]
+    test_classes = classes[cv*korak:(cv+1)*korak] 
+
+    print "trazim parametre za RBF jezgru..."
+    ispuni_datoteku('podaci')
+
+    #RBF kernel i implementirani grid
+    #ukoliko ne zelite traziti parametre, ovu liniju koda zakomentirajte i dolje direktno upisite zeljene vrijednosti
+    ratetest, paramtest = find_parameters('PUT_DO_DIREKTORIJA\podaci', '-log2c 0,4,2 -log2g -8,-4,2')
+    px=svm_problem(train_classes,train_set)
+    param = svm_parameter()
+    param.kernel_type = RBF
+    param.C = paramtest['c'] #ovdje mozete direktno upisati zeljene vrijednosti (npr param.C=1)
+    param.gamma= paramtest['g'] #ovdje mozete direktno upisati zeljene vrijednosti
+    print "treniram..."
+    m = svm_train(px, param)
+    
+    print "testiram..."
+    tocnost=svm_predict(test_classes,test_set,m)
+    print "tocnost...", tocnost[1][0]
+
+    #Linear kernel i nas grid
+    print "trazim parametre za linearnu jezgru..."
+    #ukoliko ne zelite traziti parametre, ovu liniju koda zakomentirajte i dolje direktno upisite zeljene vrijednosti
+    [a,b]=nas_grid(train_classes,train_set,[],[],4,-6,4,2,0,0,2,0)
+    px=svm_problem(train_classes,train_set)
+    param = svm_parameter('-t 0')
+    param.C = a #ovdje mozete direktno upisati zeljene vrijednosti (npr param.C=1)
+    param.gamma=b #primjer za opæi sluèaj, kod linearne jezgre nepotrebno
+    print "treniram..."
+    m = svm_train(px, param)
+    print "testiram..."
+    tocnost = svm_predict(test_classes,test_set,m)
+    print "tocnost...", tocnost[1][0]
+    
